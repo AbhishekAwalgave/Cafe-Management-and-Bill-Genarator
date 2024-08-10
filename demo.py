610dfda474d093eaeac1ddebc3c50889450837b4
AbhishekAwalgave <abhishekawalgave@gmail.com>
2024-08-10 21:17:58 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ menu = {
     "Pasta":50,
     "Burger":60,
     "Salad":70,
-    "Coffe":80,
+    "Coffee":80,
 }
 print("Welcome to Rush Cafe")
 print("Pizza:Rs40\nPasta:Rs50\nBurger:Rs60\nSalad:Rs70\nCoffe:Rs80")
